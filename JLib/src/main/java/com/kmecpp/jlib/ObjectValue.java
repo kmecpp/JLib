dbfd9bffb1f0a3980ba6c91e1700e8defb96ee71
kmecpp <kmecpp@gmail.com>
2016-08-13 23:52:55 -0400

    Add ObjectValue

---
+++ JLib/src/main/java/com/kmecpp/jlib/ObjectValue.java
@@ -0,0 +1,67 @@
+package com.kmecpp.jlib;
+
+public class ObjectValue {
+
+	private final Object object;
+
+	private ObjectValue(Object object) {
+		this.object = object;
+	}
+
+	public static ObjectValue of(Object object) {
+		return new ObjectValue(object);
+	}
+
+	public boolean isNull() {
+		return object == null;
+	}
+
+	public String asString() {
+		return (String) object;
+	}
+
+	public String asString(String def) {
+		return isNull() ? def : asString();
+	}
+
+	public boolean asBoolean() {
+		return (boolean) object;
+	}
+
+	public boolean asBoolean(boolean def) {
+		return isNull() ? def : asBoolean();
+	}
+
+	public int asInt() {
+		return (int) object;
+	}
+
+	public int asInt(int def) {
+		return isNull() ? def : asInt();
+	}
+
+	public long asLong() {
+		return (long) object;
+	}
+
+	public long asLong(long def) {
+		return isNull() ? def : asLong();
+	}
+
+	public float asFloat() {
+		return (float) object;
+	}
+
+	public float asFloat(float def) {
+		return isNull() ? def : asFloat();
+	}
+
+	public double asDouble() {
+		return (double) object;
+	}
+
+	public double asDouble(double def) {
+		return isNull() ? def : asDouble();
+	}
+
+}

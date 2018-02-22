1b3e6d7bbf32d89cbd7f3ef723f2c3b5476eca8c
kmecpp <kmecpp@gmail.com>
2018-02-22 13:43:42 -0500

    Comment out Configuration

--- src/main/java/com/kmecpp/jlib/resource/config/Configuration.java
+++ src/main/java/com/kmecpp/jlib/resource/config/Configuration.java
@@ -1,57 +1,50 @@
-//package com.kmecpp.jlib.resource.config;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.lang.reflect.Field;
-//import java.util.HashSet;
-//
-//import com.kmecpp.jlib.utils.IOUtil;
-//
-//public abstract class Configuration {
-//
-//	private File file;
-//	private HashSet<Field> deleted = new HashSet<>();
-//
-//	public Configuration() {
-//		this.file = new File(getPath());
-//	}
-//
-//	public abstract String getPath();
-//
-//	public ConfigFormat getFormat() {
-//		return ConfigFormat.JSON;
-//	}
-//
-//	public void start() throws IOException {
-//		if (!file.exists()) {
-//			save();
-//		}
-//		load();
-//	}
-//
-//	public File getFile() {
-//		return file;
-//	}
-//
-//	public void load() throws IOException {
-//		getFormat().load(this);
-//	}
-//
-//	public void save() throws IOException {
-//		getFormat().save(this);
-//	}
-//
-//	public void addDeletedField(Field field) {
-//		deleted.add(field);
-//	}
-//
-//	public boolean isDeleted(Field field) {
-//		return deleted.contains(field);
-//	}
-//
-//	public void drop() throws IOException {
-//		IOUtil.copyFile(file, new File(file.getParentFile(), file.getName() + ".broken." + System.currentTimeMillis()));
-//		load();
-//	}
-//
-//}
+package com.kmecpp.jlib.resource.config;
+
+public abstract class Configuration {
+
+	//	private File file;
+	//	private HashSet<Field> deleted = new HashSet<>();
+	//
+	//	public Configuration() {
+	//		this.file = new File(getPath());
+	//	}
+	//
+	//	public abstract String getPath();
+	//
+	//	public ConfigFormat getFormat() {
+	//		return ConfigFormat.JSON;
+	//	}
+	//
+	//	public void start() throws IOException {
+	//		if (!file.exists()) {
+	//			save();
+	//		}
+	//		load();
+	//	}
+	//
+	//	public File getFile() {
+	//		return file;
+	//	}
+	//
+	//	public void load() throws IOException {
+	//		getFormat().load(this);
+	//	}
+	//
+	//	public void save() throws IOException {
+	//		getFormat().save(this);
+	//	}
+	//
+	//	public void addDeletedField(Field field) {
+	//		deleted.add(field);
+	//	}
+	//
+	//	public boolean isDeleted(Field field) {
+	//		return deleted.contains(field);
+	//	}
+	//
+	//	public void drop() throws IOException {
+	//		IOUtil.copyFile(file, new File(file.getParentFile(), file.getName() + ".broken." + System.currentTimeMillis()));
+	//		load();
+	//	}
+
+}
